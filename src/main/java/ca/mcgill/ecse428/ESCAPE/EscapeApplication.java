35381f1f1696303a1fdfa59dfdfac9484fb703c0
Roxanne Archambault <rarch@Roxannes-MacBook-Pro.local>
2023-02-07 16:46:32 -0500

    Initialized Spring repo

---
+++ src/main/java/ca/mcgill/ecse428/ESCAPE/EscapeApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse428.ESCAPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscapeApplication.class, args);
+	}
+
+}
