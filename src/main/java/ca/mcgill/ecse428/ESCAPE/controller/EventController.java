574d769e3c7e5e42b55690f94309aa2e038809d2
jeanine56 <97963882+jeanine56@users.noreply.github.com>
2023-02-18 17:57:50 -0500

    EventController and EventRepository classes
    Empty so far but made the eventcontroller and eventrepo classes

---
+++ src/main/java/ca/mcgill/ecse428/ESCAPE/controller/EventController.java
@@ -0,0 +1,11 @@
+package ca.mcgill.ecse428.ESCAPE.controller;
+
+import ca.mcgill.ecse428.ESCAPE.EscapeApplication;
+import ca.mcgill.ecse428.ESCAPE.model.Event;
+import ca.mcgill.ecse428.ESCAPE.model.Photo;
+
+public class EventController {
+
+	
+	
+}
---
+++ src/main/java/ca/mcgill/ecse428/ESCAPE/repository/EventRepository.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse428.ESCAPE.repository;
+
+public class EventRepository {
+
+}
