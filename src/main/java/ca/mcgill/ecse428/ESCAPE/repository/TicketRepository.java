82b022e5baf2837c24f0d64b6ef9bba98885e916
Nima Hemati <nima.hemati@mail.mcgill.ca>
2023-02-24 13:59:45 -0500

    New commits

--- src/main/java/ca/mcgill/ecse428/ESCAPE/repository/TicketRepository.java
+++ src/main/java/ca/mcgill/ecse428/ESCAPE/repository/TicketRepository.java
@@ -1,9 +1,8 @@
 package ca.mcgill.ecse428.ESCAPE.repository;
 
-import ca.mcgill.ecse428.ESCAPE.model.Attendee;
-import ca.mcgill.ecse428.ESCAPE.model.UserProfile;
+import ca.mcgill.ecse428.ESCAPE.model.Ticket;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AttendeeRepository extends CrudRepository<Attendee, String> {
-    public Attendee findAttendeeByEmail(String email);
+public interface TicketRepository extends CrudRepository<Attendee, String> {
+    public Ticket findTicketByTicketId(int ticketId);
 }
