4ef5e16397680a22d7c6e14a540bc64587aa5abc
mohdels <mohammed.elsayed2@mail.mcgill.ca>
2023-02-25 13:28:20 -0500

    fixed something in the DTOs of the Photo Controller

--- src/main/java/ca/mcgill/ecse428/ESCAPE/dto/PhotoRequestDto.java
+++ src/main/java/ca/mcgill/ecse428/ESCAPE/dto/PhotoRequestDto.java
@@ -5,15 +5,18 @@ import java.util.List;
 
 public class PhotoRequestDto {
     private int photoId;
+    private String photoPath;
     private int galleryId;
     private int eventId;
 
     public int getPhotoId() { return photoId; }
 
     public void setPhotoId(int photoId) { this.photoId = photoId; }
-    public int getGalleryId() {
-        return galleryId;
-    }
+
+    public String getPhotoPath() { return photoPath; }
+
+    public void setPhotoPath(String photoPath) { this.photoPath = photoPath; }
+    public int getGalleryId() { return galleryId; }
 
     public void setGalleryId(int galleryId) {
         this.galleryId = galleryId;
