be20e3222c110071df5c8c2f843f7561cab1da33
Murad Gohar <murad.gohar@mail.mcgill.ca>
2023-02-26 16:48:54 -0500

    LEZGOO

--- escape-frontend/src/pages/HomePage.vue
+++ escape-frontend/src/pages/HomePage.vue
@@ -1,160 +1,183 @@
-<!-- This is the homepage. The main landing page for the website. -->
+<!-- This is the homepage. The main landing page for the website. --> 
 <template>
-  <section id="hero"> <!-- Big Hero Image to grab attention-->
-    <v-parallax dark src="@/assets/logo.png" height="750"> <!-- Image getter and properties-->
-      <v-row align="center" justify="center"> <!-- alignment and justification-->
-        <v-col cols="10">
+  <section id="hero"> <!-- Big Hero Image to grab attention--> 
+      <v-parallax dark src="@/assets/ESC1.jpeg" height="750"> <!-- Image getter and properties--> 
+          <v-row align="center" justify="center"> <!-- alignment and justification--> 
+              <v-col cols="10">
+                  <v-row align="center" justify="center">
+                      <v-col cols="12" md="6" xl="8">
+                          <v-app-bar app elevation="4"> <!-- Main Nav Bar--> 
+                              <v-toolbar-title>Engineering Socials Committee (ESC)
+                                  :)</v-toolbar-title>
+                              <v-spacer></v-spacer>
+                              <v-btn align="center" justify="center" color="dark" href="/login"> <!-- Login Button --> 
+                                  Login 
+                              </v-btn>
+                          </v-app-bar>
+                          <br />
+                          <br />
+                          <v-btn rounded large light @click="$vuetify.goTo('#features')" class="mt-5"> <!-- Buy tickets button --> 
+                              Buy Tickets
+                          </v-btn>
+                      </v-col>
+                      <v-col cols="12" md="6" xl="4" class="hidden-sm-and-down"> </v-col>
+                  </v-row>
+              </v-col>
+          </v-row>
+          <div class="svg-border-waves text-white"> <!-- stylistic rendering. Bootstrap used here  -->
+              <v-img src="@/assets/borderWaves.svg" />
+          </div>
+      </v-parallax>
+      <v-container fluid id="features" class="mt-2">
           <v-row align="center" justify="center">
-            <v-col cols="12" md="6" xl="8">
-              <v-app-bar app elevation="4"> <!-- Main Nav Bar-->
-                <v-toolbar-title>Welcome to Marwan's Fun House. Come Inside, it's fun inside
-                  :)</v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-btn align="center" justify="center" color="dark" href="/login"> <!-- Login Button -->
-                  Login
-                </v-btn>
-              </v-app-bar>
-              <br />
-              <br />
-              <v-btn rounded large light @click="$vuetify.goTo('#features')" class="mt-5"> <!-- Buy tickets button -->
-                Plan your Journey
-              </v-btn>
-            </v-col>
-            <v-col cols="12" md="6" xl="4" class="hidden-sm-and-down"> </v-col>
+              <v-col cols="12">
+                  <v-row align="center" justify="space-around">
+                      <v-col cols="12" class="text-center">
+                          <h1 class="font-weight-light display-2"> Engineering Socials Committee</h1> <!-- Main heading-->
+                          <h1 class="font-weight-light"> <!-- Main subheading-->
+                              Bringing you the best events of the Year!
+                          </h1>
+                          <br />
+                          <br />
+                          <br />
+                      </v-col>
+                      <v-col cols="12" sm="4" class="text-center" v-for="(feature, i) in features" :key="i" @click="redirect" style="cursor: pointer;">
+                          <v-hover v-slot:default="{ hover }"> <!-- hover feature -->
+                              <v-card class="card" shaped :elevation="hover ? 10 : 4" :class="{ up: hover }">  <!-- cards being created -->
+                                  <v-img :src="feature.img" max-width="100px" class="d-block ml-auto mr-auto"
+                                      :class="{ 'zoom-efect': hover }"></v-img>
+                                  <h1 class="font-weight-regular">{{ feature.title }}</h1>
+                                  <h4 class="font-weight-regular subtitle-1">
+                                      {{ feature.text }}
+                                  </h4>
+                              </v-card>
+                          </v-hover>
+                      </v-col>
+                  </v-row>
+              </v-col>
           </v-row>
-        </v-col>
-      </v-row>
-    </v-parallax>
-    <v-container fluid id="features" class="mt-2">
-      <v-row align="center" justify="center">
-        <v-col cols="12">
-          <v-row align="center" justify="space-around">
-            <v-col cols="12" class="text-center">
-              <h1 class="font-weight-light display-2"> Marwan's Fun House!</h1> <!-- Main heading-->
-              <h1 class="font-weight-light"> <!-- Main subheading-->
-                Come get cultured!
-              </h1>
-              <br />
-              <br />
-              <br />
-            </v-col>
-            <v-col cols="12" sm="4" class="text-center" v-for="(feature, i) in features" :key="i" @click="redirect" style="cursor: pointer;">
-              <v-hover v-slot:default="{ hover }"> <!-- hover feature -->
-                <v-card class="card" shaped :elevation="hover ? 10 : 4" :class="{ up: hover }">  <!-- cards being created -->
-                  <v-img :src="feature.img" max-width="100px" class="d-block ml-auto mr-auto"
-                         :class="{ 'zoom-efect': hover }"></v-img>
-                  <h1 class="font-weight-regular">{{ feature.title }}</h1>
-                  <h4 class="font-weight-regular subtitle-1">
-                    {{ feature.text }}
-                  </h4>
-                </v-card>
-              </v-hover>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container>
-    <v-dialog v-model="dialog" max-width="640px">
-      <v-card>
-        <youtube :video-id="videoId" @ready="ready" @playing="playing"></youtube> <!-- Left in for future functionality-->
-      </v-card>
-    </v-dialog>
-    <v-footer class="blue darken-2">
-      <v-layout row wrap align-center>
-        <v-flex xs12 class="text-xs-center">
-          <!-- Mainly Bootstrap from here-->
-          <div id="contact" class="half-height bg-dark p-5"> <!-- Section containing secondary image + text-->
-            <div class="container center center">
-              <div class="row pb-0">
-                <div class="text-center">
-                  <h2 class="display-1 white">
-                    <br />
-                    <br />
-                    <br />
-                    Bringing the World Great Art since 1969
-                  </h2>
-                </div>
-              </div>
-            </div>
-          </div>
-          <h3 class="display-1 white">
-          </h3>
-          <div id="white" class="half-height bg-dark p-5"> <!-- Secondary Section containing secondary image + text -->
-            <div class="container center center">
-              <div class="row pb-0">
-                <div class="text-center">
+      </v-container>
+      <v-dialog v-model="dialog" max-width="640px">
+          <v-card>
+              <youtube :video-id="videoId" @ready="ready" @playing="playing"></youtube> <!-- Left in for future functionality-->
+          </v-card>
+      </v-dialog>
+      <div class="svg-border-waves"> <!-- Bootstrap used to expand functionality-->
+          <img src="~@/assets/wave2.svg" />
+      </div>
+      <v-footer class="blue darken-2">
+          <v-layout row wrap align-center>
+              <v-flex xs12 class="text-xs-center">
+                  <!-- Mainly Bootstrap from here-->
+                  <div id="contact" class="half-height bg-dark p-5"> <!-- Section containing secondary image + text-->
+                      <div class="container center center">
+                          <div class="row pb-0">
+                              <div class="text-center">
+                                  <h2 class="display-1 white">
+                                      <br />
+                                      <br />
+                                      <br />
+                                      By Engineers, For Everyone
+                                  </h2>
+                              </div>
+
+                          </div>
+
+                      </div>
+                  </div>
                   <h3 class="display-1 white">
-                    <br />
-                    Enriching Lives, Expanding Minds
                   </h3>
-                </div>
+                  <div id="white" class="half-height bg-dark p-5"> <!-- Secondary Section containing secondary image + text -->
+                      <div class="container center center">
+                          <div class="row pb-0">
+                              <div class="text-center">
+                                  <h3 class="display-1 white">
+                                      <br />
+                                      Live, Love, Laugh
+                                  </h3>
+                              </div>
 
-              </div>
+                          </div>
 
-            </div>
-          </div>
-          <h3 class="display-1 white">
-          </h3>
-
-          <div id="footer" class="footer-height bg-dark p-5"> <!--  footer section, footer is hardcoded static image-->
-            <div class="container center center">
-              <div class="row pb-0">
-                <div class="text-center">
+                      </div>
+                  </div>
                   <h3 class="display-1 white">
-
                   </h3>
-                </div>
 
-              </div>
+                  <div id="footer" class="footer-height bg-dark p-5"> <!--  footer section, footer is hardcoded static image-->
+                      <div class="container center center">
+                          <div class="row pb-0">
+                              <div class="text-center">
+                                  <h3 class="display-1 white">
 
-            </div>
-          </div>
-        </v-flex>
-      </v-layout>
-    </v-footer>
+                                  </h3>
+                              </div>
+
+                          </div>
+
+                      </div>
+                  </div>
+              </v-flex>
+          </v-layout>
+      </v-footer>
   </section>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      dialog: false,
-      videoId: "i8IvvHJssWE",
-      features: [ //data for cards
-      ],
-    };
+      return {
+          dialog: false,
+          videoId: "i8IvvHJssWE",
+          features: [ //data for cards
+              {
+                  img: require("@/assets/ESC2.jpeg"),
+                  title: "Our Events",
+                  text: "Come Explore our Past and Future Events",
+              },
+              {
+                  img: require("@/assets/ESC3.jpeg"),
+                  title: "Meet The Team",
+                  text: "Meet the team that works tirelessly to bring you the best events possibly",
+              },
+              {
+                  img: require("@/assets/ESC4.jpeg"),
+                  title: "Photo Gallery",
+                  text: "Find yourself lost in our events",
+              },
+          ],
+      };
   },
   watch: {
-    dialog(value) {
-      if (!value) {
-        this.pause();
-      }
-    },
+      dialog(value) {
+          if (!value) {
+              this.pause();
+          }
+      },
   },
   methods: {
-    ready(event) {      //not yet implemented. Left in for future feature expansion
-      this.player = event.target;
-    },
-    playing() {
-      // The player is playing a video.
-    },
-    change() {
-      // when you change the value, the player will also change.
-      // If you would like to change `playerVars`, please change it before you change `videoId`.
-      // If `playerVars.autoplay` is 1, `loadVideoById` will be called.
-      // If `playerVars.autoplay` is 0, `cueVideoById` will be called.
-      this.videoId = "another video id";
-    },
-    stop() {
-      this.player.stopVideo();
-    },
-    pause() {
-      this.player.pauseVideo();
-    },
-    redirect(){
-      window.location = "/visitor";
-    }
+      ready(event) {      //not yet implemented. Left in for future feature expansion
+          this.player = event.target;
+      },
+      playing() {
+          // The player is playing a video.
+      },
+      change() {
+          // when you change the value, the player will also change.
+          // If you would like to change `playerVars`, please change it before you change `videoId`.
+          // If `playerVars.autoplay` is 1, `loadVideoById` will be called.
+          // If `playerVars.autoplay` is 0, `cueVideoById` will be called.
+          this.videoId = "another video id";
+      },
+      stop() {
+          this.player.stopVideo();
+      },
+      pause() {
+          this.player.pauseVideo();
+      },
+      redirect(){
+          window.location = "/visitor";
+      }
   },
 };
 </script>
@@ -170,6 +193,7 @@ export default {
 
 h2 {
   color: whitesmoke;
+  font: x-large;
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
   font-style: normal;
   font-weight: bold;
@@ -177,6 +201,7 @@ h2 {
 
 h3 {
   color: black;
+  font: x-large;
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
   font-style: normal;
   font-weight: bold;
@@ -192,6 +217,7 @@ h4 {
 
 #contact {
   background-color: aliceblue;
+  background: url('../assets/ESC5.jpeg') no-repeat center center;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
@@ -200,6 +226,7 @@ h4 {
 
 #white {
   background-color: aliceblue;
+  background: url('../assets/ESC6.png') no-repeat center center;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
@@ -208,6 +235,7 @@ h4 {
 
 #footer {
   background-color: aliceblue;
+  background: url('../assets/ESC7.jpeg') no-repeat center center;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
@@ -227,47 +255,47 @@ h4 {
   -webkit-transition: all 0.5s ease;
 
   .triangle {
-    -webkit-transition: all 0.7s ease-in-out;
-    stroke-dasharray: 240;
-    stroke-dashoffset: 480;
-    stroke: white;
-    transform: translateY(0);
+      -webkit-transition: all 0.7s ease-in-out;
+      stroke-dasharray: 240;
+      stroke-dashoffset: 480;
+      stroke: white;
+      transform: translateY(0);
   }
 
   &:hover {
-    .triangle {
-      stroke-dashoffset: 0;
-      opacity: 1;
-      stroke: white;
-      animation: nudge 0.7s ease-in-out;
+      .triangle {
+          stroke-dashoffset: 0;
+          opacity: 1;
+          stroke: white;
+          animation: nudge 0.7s ease-in-out;
 
-      @keyframes nudge {
-        0% {
-          transform: translateX(0);
-        }
+          @keyframes nudge {
+              0% {
+                  transform: translateX(0);
+              }
 
-        30% {
-          transform: translateX(-5px);
-        }
+              30% {
+                  transform: translateX(-5px);
+              }
 
-        50% {
-          transform: translateX(5px);
-        }
+              50% {
+                  transform: translateX(5px);
+              }
 
-        70% {
-          transform: translateX(-2px);
-        }
+              70% {
+                  transform: translateX(-2px);
+              }
 
-        100% {
-          transform: translateX(0);
-        }
+              100% {
+                  transform: translateX(0);
+              }
+          }
       }
-    }
 
-    .circle {
-      stroke-dashoffset: 0;
-      opacity: 1;
-    }
+      .circle {
+          stroke-dashoffset: 0;
+          opacity: 1;
+      }
   }
 }
 </style>
