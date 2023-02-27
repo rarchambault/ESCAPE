af1ba0de2d80ebe135788193b0e9214d34a7bbcc
Roxanne Archambault <roxanne.archambault@mail.mcgill.ca>
2023-02-27 17:14:59 -0500

    weird changes again

--- escape-frontend/src/main.js
+++ escape-frontend/src/main.js
@@ -2,24 +2,12 @@ import Vue from 'vue'
 import HomePage from './pages/HomePage.vue'
 import ViewAllEvents from './components/ViewAllEvents.vue' // import the ViewAllEvents component
 import vuetify from './plugins/vuetify'
-import AboutUs from './pages/AboutUs.vue'
-import TicketingPage from './pages/TicketingPage'
-import PostPage from "./pages/PostPage"
-import ViewTickets from "./pages/ViewTickets"
-import PhotoGalleries from "./pages/PhotoGalleries"
-import PhotoGallery from "./pages/PhotoGallery"
 
 Vue.config.productionTip = false
 
 const routes = {
   '/': HomePage,
   '/home': HomePage,
-  '/about': AboutUs,
-  "/ticketing": TicketingPage,
-  "/post": PostPage,
-  "/viewtickets": ViewTickets,
-  "/photogalleries": PhotoGalleries,
-  "/gallery/photogallery": PhotoGallery,
   '/events': ViewAllEvents // add a route for the ViewAllEvents component
 }
 
