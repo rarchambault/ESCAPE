b200ce0c620938a07ed7d9ebaf007cb8f8765426
ZachGodden <90073324+ZachGodden@users.noreply.github.com>
2023-02-26 22:52:35 -0500

    AboutUs Page template

--- escape-frontend/src/main.js
+++ escape-frontend/src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import HomePage from './pages/HomePage.vue'
 import vuetify from './plugins/vuetify'
+import AboutUs from './pages/AboutUs.vue'
 
 Vue.config.productionTip = false
 
 const routes = {
   '/': HomePage,
   '/home': HomePage,
+  '/about': AboutUs,
 }
 
 new Vue({
