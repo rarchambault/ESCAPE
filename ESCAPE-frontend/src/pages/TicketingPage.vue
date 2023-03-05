72cd1f1314dc8dca312698d130ddd8a9e5e7d9c4
Murad Gohar <murad.gohar@mail.mcgill.ca>
2023-03-04 20:29:57 -0500

    Ticketing middle-end

--- ESCAPE-frontend/src/pages/TicketingPage.vue
+++ ESCAPE-frontend/src/pages/TicketingPage.vue
@@ -20,7 +20,7 @@
               <v-card-subtitle>{{ event.date }}</v-card-subtitle>
               <v-card-text>{{ event.description }}</v-card-text>
               <v-card-actions>
-                <v-btn color="orange" text :href="event.ticketUrl">Buy Tickets</v-btn>
+                <v-btn color="orange" text :href="event.ticketUrl" >Buy Tickets</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
@@ -30,37 +30,21 @@
   </v-app>
 </template>
 
-
 <script>
 export default {
   name: "EventTicketingPage",
   data: () => ({
-    events: [
-      {
-        title: "Power Hour",
-        date: "April 15th, 2023",
-        description:
-            "Come join us for our famous Power Hour event, where you will enjoy an hour of drinking and music.",
-        ticketUrl: "https://mcgilleus.yapsody.com/",
-        image: require("../assets/ESC7.jpeg")
-      },
-      {
-        title: "Apartment Crawl",
-        date: "May 6th, 2023",
-        description:
-            "Join us for a tour of some of the best apartments in the city, with drinks and good times at each stop.",
-        ticketUrl: "https://mcgilleus.yapsody.com/",
-        image: require("../assets/ESC5.jpeg")
-      },
-      {
-        title: "Foam Party",
-        date: "June 10th, 2023",
-        description:
-            "Get ready to get wet and wild at our Foam Party event, featuring great music and lots of bubbles.",
-        ticketUrl: "https://mcgilleus.yapsody.com/",
-        image: require("../assets/ESC10.png")
-      }
-    ]
-  })
+    events: []
+  }),
+  mounted() {
+    this.getTickets();
+  },
+  methods: {
+    async getTickets() {
+      const response = await fetch('/api/tickets');
+      const data = await response.json();
+      this.events = data;
+    }
+  }
 };
 </script>
